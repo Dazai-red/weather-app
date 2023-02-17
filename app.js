d69c699b35c4b877926d5eef689b87ce25234822
Dazai-red <joseanlopezavila@gmail.com>
2023-02-17 22:58:53 +0100

    Añadir función getWeatherByCoords, control de errores y mostrar datos de la API

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 window.addEventListener('load', ()=>{
 
     if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(geoPosOK, geoPosKO)
+        navigator.geolocation.getCurrentPosition(geoPosOk, geoPosKo)
     }else{
-        console.log('Tu navegador no dispone la API de geolocalización')
+        console.log('Tu navegador no dispone de la API de geolocalización')
     }
 
 });
 
-function geoPosOK(pos){
+function geoPosOk(pos){
     let lat = pos.coords.latitude;
     let lon = pos.coords.longitude;
 
-    console.log(`Estas en la posición: ${lat}, ${lon} ` );
+    console.log(`Estas en la posición: ${lat}, ${lon}`);
+
+    getWeatherByCoords(lat,lon);
+
 }
 
-function geoPosKO(err){
+function geoPosKo(err){
     console.warn(err.message);
     let msg;
     switch(err.code){
@@ -33,4 +36,59 @@ function geoPosKO(err){
             break;
     }
     console.log(msg);
+}
+
+const getWeatherByCoords = async(lat, lon) =>{
+    try{
+        
+        const reply  = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=es&units=metric&appid=8888a7546eef15137503e5dea18fcd7f`)
+
+        if(getStatus(reply.status)==="succesfull"){
+            const data = await reply.json();
+            console.log(data);
+            
+            printData(data.name,data.main.temp,data.weather[0].description,data.main.feels_like,data.main.humidity,data.main.pressure,data.wind.speed,data.main.temp_max,data.main.temp_min);
+
+        }else{
+            // imprimimos los errores con la función imprimir errores
+            console.log(getStatus(reply.status));
+        }
+
+    } catch(error){
+        console.warn(error.message);
+    }
+}
+
+const getStatus = (status)=>{
+    if(status === 200){
+        return "succesfull";
+    }else if(status === 401){
+        return "ERROR: API KEY INVALIDA";
+    }else if(status === 404){
+        return "ERROR: La API no contiene este recurso";
+    }else if(status === 400){
+        return "ERROR: Ciudad no valida";
+    }else{
+        return "ERROR DESCONOCIDO.";
+    }
+}
+
+const printData = (city,temp,desc,feels,humidity,pressure,windSpeed,tempMax,tempMin)=>{
+    // nombre de la ciudad
+    console.log(city)
+    // temperatura
+    console.log(`Temperatura: ${Math.round(temp)}°C`)
+    //Descripción del clima
+    console.log(desc)
+    //sensación térmica
+    console.log(`Sensación térmica: ${Math.round(feels)}°C`)
+    // Humedad
+    console.log(`Humedad: ${humidity}%`)
+    // presión
+    console.log(`Presión: ${pressure}mbar`)
+    // velocidad de viento
+    console.log(`Velocidad de viento: ${windSpeed} m/s`)
+    // temperatura máx y min
+    console.log(`Temp Max: ${Math.round(tempMax)}°C`)
+    console.log(`Temp Min: ${Math.round(tempMin)}°C`)
 }
