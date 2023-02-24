a8eab84b643a7d28adf805568e777cae9e4ed52c
Dazai-red <joseanlopezavila@gmail.com>
2023-02-24 19:57:42 +0100

    Terminada la lógica en JS

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
 window.addEventListener('load', ()=> {
 
+    background = document.getElementById('background');
     buttonOpenRecord = document.getElementById('open-record')
     buttonCloseRecord = document.getElementById('close-record');
     buttonClearRecord = document.getElementById('clear-record');
     windowRecord = document.getElementById('window-record');
     containerRecord = document.getElementById('record-container');
     
-    
     windowErrors = document.getElementById('window-errors');
     buttonCloseErrors = document.getElementById('close-errors');
     containerErrors = document.getElementById('errors-container');
+
+    searchForm = document.getElementById('search-form');
+    cityInput = document.getElementById('city-input');
+
+    converterButton = document.getElementById('converter');
 
     buttonOpenRecord.addEventListener('click', () => {
         containerRecord.innerHTML = getDataFromBrowser();
@@ -29,10 +34,55 @@ window.addEventListener('load', ()=> {
         windowErrors.classList.remove('window-errors--open');   
     });
 
+    searchForm.addEventListener('submit', event =>{
+        event.preventDefault();
+        console.log(cityInput.value);
+        getWeatherByCity(cityInput.value);
+    });
+
+    converterButton.addEventListener('click', () => {
+        
+        // antes de hacer una petición a la api estos span no existen
+        const valueTemp = document.getElementById('valueTemp');
+        const extentTemp = document.getElementById('extentTemp');
+        const valueTempMax = document.getElementById('valueTempMax');
+        const extentTempMax = document.getElementById('extentTempMax');
+        const valueTempMin = document.getElementById('valueTempMin');
+        const extentTempMin = document.getElementById('extentTempMin');
+        const valueFeels = document.getElementById('valueFeels');
+        const extentFeels = document.getElementById('extentFeels');
+
+        let pressed = converterButton.classList.contains('pressed');
+        // Si no le damos permiso de geo no tendremos estos span generados 
+        if(valueTemp!=null){
+            if(pressed){
+                converterButton.classList.remove('pressed');
+                valueTemp.textContent = Math.round(getFahrenheitToCelsius(valueTemp.innerHTML));
+                extentTemp.textContent = '°C';
+                valueTempMax.textContent = Math.round(getFahrenheitToCelsius(valueTempMax.innerHTML));
+                extentTempMax.textContent = '°C';
+                valueTempMin.textContent = Math.round(getFahrenheitToCelsius(valueTempMin.innerHTML));
+                extentTempMin.textContent = '°C';
+                valueFeels.textContent = Math.round(getFahrenheitToCelsius(valueFeels.innerHTML));
+                extentFeels.textContent = '°C';
+            }else{
+                converterButton.classList.add('pressed');
+                valueTemp.textContent = Math.round(getCelsiusToFahrenheit(valueTemp.innerHTML));
+                extentTemp.textContent = '°F';
+                valueTempMax.textContent = Math.round(getCelsiusToFahrenheit(valueTempMax.innerHTML));
+                extentTempMax.textContent = '°F';
+                valueTempMin.textContent = Math.round(getCelsiusToFahrenheit(valueTempMin.innerHTML));
+                extentTempMin.textContent = '°F';
+                valueFeels.textContent = Math.round(getCelsiusToFahrenheit(valueFeels.innerHTML));
+                extentFeels.textContent = '°F';
+            }
+        }
+    });
+
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(geoPosOk, geoPosKo);
     }else{
-        console.log('Tu navegador no dispone de la API de geolocalización');
+        printErrors('Tu navegador no dispone de la API de geolocalización');
     }
 
 
@@ -81,18 +131,55 @@ const getWeatherByCoords = async(lat, lon) =>{
         const reply  = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=es&units=metric&appid=`)
 
         if(getStatus(reply.status)==="succesfull"){
+            
             const data = await reply.json();
             
             console.log(data);
             
+            // imprime los datos 
             printData(data.name,data.main.temp,data.weather[0].description,data.main.feels_like,data.main.humidity,data.main.pressure,data.wind.speed,data.main.temp_max,data.main.temp_min,data.weather[0].main);
 
+            // Pasa los datos al LocalStorage del navegador del cliente
             setDataInBrowser(data.name,data.main.temp,data.main.temp_max,data.main.temp_min)
 
-
+            // le pasamos el dataTime de cuando se hizo la petición
+            setBackgroundImage(data.dt);
 
         }else{
             // imprimimos los errores con la función imprimir errores
+            console.warn(getStatus(reply.status));
+            printErrors(getStatus(reply.status));
+        }
+
+    } catch(error){
+        console.warn(error.message);
+        printErrors(error.message);
+    }
+}
+
+const getWeatherByCity = async (city) => {
+    try{
+        
+        const reply  = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=es&units=metric&appid=`)
+
+        if(getStatus(reply.status)==="succesfull"){
+            
+            const data = await reply.json();
+            
+            console.log(data);
+            
+            // imprime los datos 
+            printData(data.name,data.main.temp,data.weather[0].description,data.main.feels_like,data.main.humidity,data.main.pressure,data.wind.speed,data.main.temp_max,data.main.temp_min,data.weather[0].main);
+
+            // Pasa los datos al LocalStorage del navegador del cliente
+            setDataInBrowser(data.name,data.main.temp,data.main.temp_max,data.main.temp_min)
+
+            // le pasamos el dataTime de cuando se hizo la petición
+            setBackgroundImage(data.dt);
+
+        }else{
+            // imprimimos los errores con la función imprimir errores
+            console.warn(getStatus(reply.status));
             printErrors(getStatus(reply.status));
         }
 
@@ -106,11 +193,11 @@ const getStatus = (status)=>{
     if(status === 200){
         return "succesfull";
     }else if(status === 401){
-        return "ERROR: API KEY INVALIDA";
+        return "ERROR 401: API KEY INVALIDA";
     }else if(status === 404){
-        return "ERROR: La API no contiene este recurso";
+        return "ERROR 404: Ciudad no valida";
     }else if(status === 400){
-        return "ERROR: Ciudad no valida";
+        return "ERROR 400: No has introducido ciudad";
     }else{
         return "ERROR DESCONOCIDO.";
     }
@@ -157,13 +244,31 @@ const printData = (city,temp,desc,feels,humidity,pressure,windSpeed,tempMax,temp
 
 const getDateTime = () =>{
 
+    /* 
+        Otra forma de hacerlo pero más compleja:
+        let date = new Date().toLocaleString("es-ES", {
+            timeStyle: "long",
+            dateStyle: "short",
+            hour12: true
+        });
+        console.log(date); Nos daria el siguiente formato: 21/2/23, 1:11:03 p. m. CET
+    */
+
     const now = new Date();
     let hour = now.getHours();
     let min = now.getMinutes();
+    let system = '';
 
     const dayWeek = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
 
     const monthYear = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
+    
+    
+    if(hour > 12){
+        system = 'PM'
+    }else{
+        system = 'AM'
+    }
 
     hour = hour % 12;
     if(hour < 10)
@@ -172,7 +277,7 @@ const getDateTime = () =>{
         min = "0" + min;
     }
 
-    return `${dayWeek[now.getDay()]} ${now.getDate()} de ${monthYear[now.getMonth()]} de ${now.getFullYear()}, ${hour}:${min}`;
+    return `${dayWeek[now.getDay()]} ${now.getDate()} de ${monthYear[now.getMonth()]} de ${now.getFullYear()}, ${hour}:${min} ${system}`;
 }
 
 
@@ -219,7 +324,7 @@ const setDataInBrowser =  (city,temp,tempMax,tempMin) => {
             record = localItems;
         }
         record.push(`<div class='item'><div class='item__city'>${city}</div><div class='item__temps'>${tempMax} / ${tempMin}</div><div class='item__main-temp'>${temp}</div></div>`);
-        localStorage.setItem('localitem', JSON.stringify(record))
+        localStorage.setItem('localItem', JSON.stringify(record))
         console.log('Se guardaron los datos correctamente en LocalStorage')
 
     }else{
@@ -232,26 +337,62 @@ const setDataInBrowser =  (city,temp,tempMax,tempMin) => {
 const getDataFromBrowser = () =>{ 
     if(typeof(Storage)!== "undefined"){
         
+        let localItems = JSON.parse(localStorage.getItem('localItem'))
+        
         let outPut = '';
-        let localItems = JSON.parse(localStorage.getItem('localitem'));    
-        // console.log(typeof(localItems));
+
+        console.log(localItems);
 
         if(localItems === null){
-            record = []
+            arrayRecord = []
         }else{
-            record = localItems;
+            arrayRecord = localItems;
         }
 
-        record.forEach((data, index) => {
-            outPut += `${data}`;
+        arrayRecord.forEach((content, index) => {
+            outPut += content;
         });
 
         return outPut;
 
     }else{
-        return 'Su navegador no soporta LocalStorage';
+        printErrors('Su navegador no soporta LocalStorage');
+        return '';
     }
 }
+
+const setBackgroundImage = (dateTime) =>{
+    /* 
+        dateTime es un número que indica:
+        Tiempo Unix o Tiempo POSIX es un sistema para la 
+        descripción de instantes de tiempo: se define como 
+        la cantidad de segundos transcurridos desde la 
+        medianoche UTC del 1 de enero de 1970, sin contar 
+        segundos intercalares.
+    */
+    // convierte tiempo unix a una hora entendible 
+    const dayHour = new Date(dateTime*1000).getHours();
+    // console.log(dayHour);
+    if(dayHour > 6 && dayHour < 18){
+        background.classList.remove('night');
+        background.classList.add('day');
+    }else{
+        background.classList.remove('day');
+        background.classList.add('night');
+    }
+}
+
+const getCelsiusToFahrenheit = (celsius) =>{
+    return celsius * 9 / 5.0 + 32;
+}
+
+const getFahrenheitToCelsius = (fahrenheit) =>{
+    return (fahrenheit - 32) * 5 / 9.0;
+}
+
+
+
+
 
 
 
